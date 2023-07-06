b3f9c7d972186d2db592525cf76ce61b6616a38b
Wahyu <wahyu@gmail.com>
2023-07-06 15:47:15 +0700

    update html and css 2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var slideIndex = 1
+setInterval(function(){
+    plusDiv(1)
+}, 1500)
+
+function plusDiv(index){
+    showImage(slideIndex += index)
+}
+
+function showImage(index) {
+    console.log(index)
+    const imglist = document.getElementsByClassName("slider")
+    if (index > imglist.length) {slideIndex = 1}
+    if (index < 1) {slideIndex = imglist.length}
+    for (i = 0; i < imglist.length; i++) {
+        imglist[1].style.display = "none"
+    }
+    imglist[slideIndex - 1].style.display = "block"
+}
